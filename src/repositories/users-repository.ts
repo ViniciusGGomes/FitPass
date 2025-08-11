4f80fb6c9d0aa48268ee621b48e876a67c0edfae
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-08-11 22:55:30 +0000

    feat(users-repository): add user repository interface

---
+++ src/repositories/users-repository.ts
@@ -0,0 +1,10 @@
+import { Prisma, User } from "@prisma/client";
+
+export interface UserRepository {
+  findByEmail(email: string): Promise<User | null>;
+  create(data: Prisma.UserCreateInput): Promise<User>;
+}
+
+// A interface vai disser quais métodos(create) vão existir no meu repositório, e quais parâmetros esse método vai receber. Exemplo: método -> create. e ele devolve uma promise(Vai demorar para fazer) que é o User
+
+// Recebo o email, e devolvo o usuário, mas se não encontrar a promise vai devolver um usuário ou null caso não tenha encontrado
