2e7d7cbbe558116fe6635c2c7467240385e7e8e3
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-08-20 21:00:11 +0000

    refactor(register): add typed response interface and return created user

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcryptjs";
 import { UserRepository } from "@/repositories/users-repository";
 import { UserAlreadyExistError } from "./Errors/user-already-exist-error";
+import { User } from "@prisma/client";
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -8,22 +9,30 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private userRepository: UserRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({ name, email, password }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6);
-
+ 
     const userWithSameEmail = await this.userRepository.findByEmail(email);
 
     if (userWithSameEmail) {
       throw new UserAlreadyExistError();
     }
 
-    await this.userRepository.create({
+    const user = await this.userRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return {
+      user,
+    };
   }
 }
