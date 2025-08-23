945c34b500e596347a100be97b7b3c7b99b6462b
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-08-23 21:40:42 +0000

    test(in-memory-user-repository): implement in-memory user repository pattern

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,29 @@
+import { User, Prisma } from "@prisma/client";
+import { UserRepository } from "../users-repository";
+
+export class InMemoryUsersRepository implements UserRepository {
+  public itens: User[] = [];
+
+  async findByEmail(email: string) {
+    const user = this.itens.find((item) => item.email === email);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: "user-1",
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.itens.push(user);
+
+    return user;
+  }
+}
