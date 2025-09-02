d6cb4e37cf72eec8abd8f1161022737910ae10fc
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-09-02 03:58:23 +0000

    feat(in-memory): Implement findById, findManyByUserId, countByUserId, and save methods

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,16 +1,16 @@
 import { Prisma, CheckIn } from "@prisma/client";
-import { CheckInRepository } from "../check-ins-repository";
+import { CheckInsRepository } from "../check-ins-repository";
 import { randomUUID } from "node:crypto";
 import dayjs from "dayjs";
 
-export class InMemoryCheckInRepository implements CheckInRepository {
-  public itens: CheckIn[] = [];
+export class InMemoryCheckInRepository implements CheckInsRepository {
+  public items: CheckIn[] = [];
 
   async findByUserIdOnDate(userId: string, date: Date) {
     const startDay = dayjs(date).startOf("date");
     const endOfTheDay = dayjs(date).endOf("date");
 
-    const checkInOnSameDate = this.itens.find((checkIn) => {
+    const checkInOnSameDate = this.items.find((checkIn) => {
       const checkInDate = dayjs(checkIn.created_at);
       const isOnSameDate =
         checkInDate.isAfter(startDay) && checkInDate.isBefore(endOfTheDay);
@@ -25,6 +25,27 @@ export class InMemoryCheckInRepository implements CheckInRepository {
     return checkInOnSameDate;
   }
 
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
+    //Preciso passar os indices que eu quero 0/20 -> vai do índice 0 ao 20
+  }
+
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length;
+  }
+
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    if (!checkIn) {
+      return null;
+    }
+
+    return checkIn;
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
@@ -34,7 +55,17 @@ export class InMemoryCheckInRepository implements CheckInRepository {
       created_at: new Date(),
     };
 
-    this.itens.push(checkIn);
+    this.items.push(checkIn);
+
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
 
     return checkIn;
   }
