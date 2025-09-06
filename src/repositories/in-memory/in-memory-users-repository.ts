4290320c44529224d5afa0f417cc2828018dad58
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-09-06 23:04:24 +0000

    refactor(in-memory): update InMemoryUsersRepository to implement UsersRepository interface

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,8 +1,8 @@
 import { User, Prisma } from "@prisma/client";
-import { UserRepository } from "../users-repository";
+import { UsersRepository } from "../users-repository";
 import { randomUUID } from "node:crypto";
 
-export class InMemoryUsersRepository implements UserRepository {
+export class InMemoryUsersRepository implements UsersRepository {
   public itens: User[] = [];
 
   async findById(id: string) {
