d69cc8639d8eee8532fff1b0cf4e277516b1de15
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-09-12 23:31:55 +0000

    feat(routes): add auth routes and protect /me with verify-jwt middleware

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from "fastify";
+
+import { verifyJwt } from "@/http/middlewares/verify-jwt";
+import { register } from "./register";
+import { authenticate } from "./authenticate";
+import { profile } from "./profile";
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post("/users", register);
+  app.post("/session", authenticate);
+
+  //** Authenticated */
+  app.get("/me", { onRequest: [verifyJwt] }, profile);
+}
