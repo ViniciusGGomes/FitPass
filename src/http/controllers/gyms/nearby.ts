096fc514aa8fdf2b1b7696332828c5d2dec703bb
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-09-13 18:45:18 +0000

    feat(controller): add nearby gyms endpoint

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,25 @@
+import { makeFetchNearbyGymUseCase } from "@/use-cases/Factory/make-fetch-nearby-gyms-use-case";
+import { FastifyRequest, FastifyReply } from "fastify";
+import z from "zod";
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.coerce.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.coerce.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.query);
+
+  const fetchNearbyGymUseCase = makeFetchNearbyGymUseCase();
+
+  const { gyms } = await fetchNearbyGymUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  reply.status(200).send({ gyms });
+}
