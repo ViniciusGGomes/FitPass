be2bba7a8a20df1c534b3ebfcabe3490e8768cd8
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-09-13 18:47:00 +0000

    test(e2e): add create gym integration test

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Create Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a gym", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const response = await request(app.server)
+      .post("/gyms")
+      .set(`Authorization`, `Bearer ${token}`)
+      .send({
+        title: "Javascript Gym",
+        description: "some description",
+        phone: "6399999999",
+        latitude: -23.5868031,
+        longitude: -46.6847268,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
